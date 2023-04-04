9cfefe8c0c9a5edb1d017d19a3fc47158997c4a7
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-04-05 02:21:22 +0530

    remic icons and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+       fontFamily: {
+      display: ['Inter', 'system-ui', 'sans-serif'],
+      body: ['Inter', 'system-ui', 'sans-serif'],
+    },
       colors: {
       primaryColor: "#8873ef",
       headingColor: "#081e21",
