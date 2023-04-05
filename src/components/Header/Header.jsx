4ab560a571da218c9ab32b3d06a13310b6f7399e
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-04-05 13:03:00 +0530

    Logo added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>HEADER</div>
+  return (
+    <header className="w-full h-[80px] loading-[80px] flex items-center">
+      <div className="container">
+        <div className=" flex items-center gap-[10px]">
+          <span className="w-[35px] h-[45px] bg-primaryColor text-white text=[18px] font-[500] rounded-full flex items-center justify-center">
+          R
+          </span>
+          <div className="leading-[20px">
+            <h2 className="text-xl text-smallTextColor font-[700]">Ekpenisi</h2>
+            <p className=" text-[14px] text-smallTextColor font-[500]">E. Raphael</p>
+          </div>
+        </div>
+      </div>
+    </header>   
+  )
 }
 
 export default Header;
