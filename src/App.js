55c0866f2235a0f167d7b449c88a45b4387207b2
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-04-05 17:40:44 +0530

    View set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useEffect } from "react";
-import Aos from "aos";
-import "./App.css";
+import { useEffect } from 'react';
+import Aos from 'aos';
+import './App.css';
 import Header from './components/Header/Header.jsx';
 import Footer from './components/Footer/Footer.jsx';
 
+import View from './components/View/View.jsx';
 const App = () => {
     useEffect(()=> {
         Aos.init();
@@ -11,6 +12,9 @@ const App = () => {
     return <>
 
     <Header />
+    <main> 
+      <View />
+     </main>
     <Footer />
     </>
        
