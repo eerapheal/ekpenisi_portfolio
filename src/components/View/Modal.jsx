2c8c43690c4c29c0a7167c3d48d9b35c67f39925
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-06-18 11:14:54 +0530

    Modal image  shaped.

--- src/components/View/Modal.jsx
+++ src/components/View/Modal.jsx
@@ -6,12 +6,22 @@ import portfolios from '../../assets/data/portfolioData';
 
 const Modal = ({activeID, setShowModal}) => {
   const portfolio = portfolios.find(portfolio => portfolio.id === activeID);
+ if (!portfolio) {
+    // console.error(`Could not find portfolio with ID ${activeID}`);
+    return null; // Or render an error message or fallback component
+  }
 
   return (
-  
-    <div>
-      <img src={ portfolio.imgUrl } alt="" />
+  <div className="w-full h-full fixed top-0 z-10 bg-headingColor bg-opacity-40">
+ <div className="max-w-[600px] absolute top-1/2 left-1/2 z-20 bg-white rounded-[8px] transform -translate-x-1/2 -translate-y-1/2 p-5">
+  <div>
+  <figure>
+  <img src={ portfolio.imgUrl } alt="" />
+  </figure> 
+  </div>
     </div>
+  </div>
+   
   );
 }
 
